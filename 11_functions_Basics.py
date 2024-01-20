f3e41963571e08d68fe640be7ec2a22d7796d0da
Limitless Playing <limitlessplaying@gmail.com>
2024-01-20 09:11:28 +0545

    Multiplication table using function

--- 11_functions_Basics.py
+++ 11_functions_Basics.py
@@ -3,12 +3,11 @@
 
 #To calculate the factorial using function
 
-def multiplication(num, i):
-    result =  num*i
-    return result
+def multiplication(num, value):
+    for i in range (1, value+1):
+        print(num, "X", i, "=" ,num*i)
 
 num = int(input("Enter a number to find its multiplication table : "))
 value = int(input("Enter a number to find its multiplication table : "))
 print("The multiplication table of ", num, " is")
-for i in range(1, value+1):
-    print(num, "X", i, "=" ,multiplication(num, i))
+multiplication(num, value)
