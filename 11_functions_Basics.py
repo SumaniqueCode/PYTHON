d8ee60b2a9a11767c0920041b8ca067813a287dc
Limitless Playing <limitlessplaying@gmail.com>
2024-01-21 19:32:42 +0545

    Default Arguments in function

--- 11_functions_Basics.py
+++ 11_functions_Basics.py
@@ -1,11 +1,12 @@
 # Created on 2024/01/20
 # By Suman Regmi
 
-#To calculate the factorial using function
 
+# To calculate the factorial using function
 def multiplication(num, value):
-    for i in range (1, value+1):
-        print(num, "X", i, "=" ,num*i)
+    for i in range(1, value + 1):
+        print(num, "X", i, "=", num * i)
+
 
 num = int(input("Enter a number to find its multiplication table : "))
 value = int(input("Enter a number to find its multiplication table : "))
@@ -14,29 +15,42 @@ multiplication(num, value)
 
 
 # Updated on 2024/01/21
-
-#To find cube of number using nested function
-
+# To find cube of number using nested function
 def cube(x):
     def square(a):
-        s = a*a
+        s = a * a
         print(s)
-        return s 
-    c = square(x)*x
+        return s
+
+    c = square(x) * x
     print(c)
 
-n= int(input("Enter the value\n"))
-cube(n) 
 
-# Updated on 2024/01/21
+n = int(input("Enter the value\n"))
+cube(n)
 
-#To find square and cube using lambda function
 
-def fun(f,n):
+# To find square and cube using lambda functio
+def fun(f, n):
     print(f(n), end=" ")
-square = lambda x: x*x
-cube = lambda x: x*x*x
 
-n= int(input("Enter any numbers: \n"))
+
+square = lambda x: x * x
+cube = lambda x: x * x * x
+
+n = int(input("Enter any numbers: \n"))
 fun(square, n)
 fun(cube, n)
+
+
+# Default Arguments in Python
+# Here default value of faculty is science,
+# if arugument is passed then it will be replaced by new value
+def display(name, age, faculty="science"):
+    print("\n Name: ", name)
+    print("Faculty: ", faculty)
+    print("Age: ", age)
+
+
+display(age=16, name="Sam Wilson")  # order or position of args is not important
+display(name="John Doe", faculty="Management", age=21)
