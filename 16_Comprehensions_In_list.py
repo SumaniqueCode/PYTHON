6bde7797ee0ca58e3a90ac7fe719c2d5a27ac62b
Limitless Playing <limitlessplaying@gmail.com>
2024-02-13 21:51:17 +0545

    Comprehensions in PYTHON

--- 16_Comprehensions_In_list.py
+++ 16_Comprehensions_In_list.py
@@ -2,7 +2,7 @@
 # By Suman Regmi
 
 
-#finds the  square of even numbers from 0 to 9 and stores them in a list.
+#finds the square of even numbers from 0 to 9 and stores them in a list.
 squares = [x**2 #for finding square
             for x in range(10) #for the loop upto 10
               if (x%2 == 0) #for checking conditions whether the x is odd or even
