bd23899a32fe0ba6048c3b85bd06afd080879515
Limitless Playing <limitlessplaying@gmail.com>
2024-02-29 16:00:52 +0545

    Error handling in Python

--- 21_Error_Handling.py
+++ 21_Error_Handling.py
@@ -10,7 +10,7 @@ def valueChecker(text):
 a = input("Enter any string: ")
 try:                            #the main logic is in try block
     print(valueChecker(a))
-except ValueError as error:        #if  any error occurs in try block, it will be caught by except block and handled accordingly.
+except ValueError as error:     #if  any error occurs in try block, it will be caught by except block and handled accordingly.
     print(error)
 else:                           #this block will execute if no errors occur in the try block. It's optional.
     print("Program Executed successfully")
