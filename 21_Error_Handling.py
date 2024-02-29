eb981ed5599f1bcbf08057bac519cc70903c87c0
Limitless Playing <limitlessplaying@gmail.com>
2024-02-29 15:59:03 +0545

    Error handling in Python

--- 21_Error_Handling.py
+++ 21_Error_Handling.py
@@ -1,12 +1,15 @@
 # Created on 2024/02/29
 # By Suman Regmi
 
-a = input("Enter any string: ")
-if(a==""):
-    raise ValueError("String cannot be empty") #throws an exception if the user enters nothing
+def valueChecker(text):
+    if(a==""):
+        raise ValueError("\nString cannot be empty") #throws an exception if the user enters nothing
+    else:
+        return "\nEntered string is \""+a+"\""
 
+a = input("Enter any string: ")
 try:                            #the main logic is in try block
-    print(a)
+    print(valueChecker(a))
 except ValueError as error:        #if  any error occurs in try block, it will be caught by except block and handled accordingly.
     print(error)
 else:                           #this block will execute if no errors occur in the try block. It's optional.
