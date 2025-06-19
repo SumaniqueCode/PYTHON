dc2cbf398c3cfe4e59acad90486d4b8f67b6d16d
Suman Regmi <limitlessplaying@gmail.com>
2025-06-19 10:46:36 +0545

    Number guessing game using python

--- 27_Number_guessing_game.py
+++ 27_Number_guessing_game.py
@@ -4,35 +4,36 @@
 
 import math
 import random
+
 operation = "y"
 while operation == "y" or operation == "Y":
     lower = int(input(f"Enter the lower limit: "))
     upper = int(input(f"Enter the upper limit (greater than {lower}): "))
-    num = random.randint(lower,upper)
+    num = random.randint(lower, upper)
 
-    #The chances varies from the range you are selecting so math.log() is used
-    chances = round(math.log(upper-lower+1,2))
+    # The chances varies from the range you are selecting so math.log() is used
+    chances = round(math.log(upper - lower + 1, 2))
     print("You have got", chances, "Chances")
 
     count = 0
-    while count<chances:
+    while count < chances:
         guess = int(input("Enter the guess: "))
-        if guess == num :
+        if guess == num:
             print(f"Congratulations! You won. The number is ", num)
             break
         else:
-            if (guess<lower or guess>upper):
+            if guess < lower or guess > upper:
                 print("The guess exceed the upper limit and lower limit")
-            elif guess<num:
+            elif guess < num:
                 print("The number is greater than the guess")
             else:
                 print("The number is less than the guess")
 
-            if(count<chances-1) :
-                print("You have ", chances-count-1, " chances left")
+            if count < chances - 1:
+                print("You have ", chances - count - 1, " chances left")
             else:
                 print("Sorry! You lost. Better luck  next time.")
                 print("The number was: ", num)
-                
+
             count += 1
     operation = input("press Y to continue or N to quiet: ")
