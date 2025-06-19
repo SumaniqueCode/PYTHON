63d62642928946e1e77d2c1c5978ac30ecf269b9
Suman Regmi <limitlessplaying@gmail.com>
2025-06-19 10:45:31 +0545

    Fix

--- 21_Error_Handling.py
+++ 21_Error_Handling.py
@@ -1,18 +1,75 @@
 # Created on 2024/02/29
 # By Suman Regmi
 
+# try except
+try:  # the main logic is in try block
+    x = 10 / 0
+except (ZeroDivisionError) as e: # if  any error occurs in try block, it will be caught by except block and handled accordingly.
+    print("Cannot divide by zero.")
+
+# multiple exception
+try:
+    value = int("abc")
+except (ValueError, TypeError):
+    print("Value or Type error occurred.")
+
+# try else
+try:
+    result = 10 / 2
+except ZeroDivisionError:
+    print("Error!")
+else:
+    print("Success:", result)
+
+# finally
+try:
+    f = open("file.txt", "r")
+except FileNotFoundError:
+    print("File not found.")
+finally:
+    print("Closing file or cleaning up resources.")
+
+
+# raising error
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+
+try:
+    divide(5, 0)
+except ValueError as e:
+    print(e)
+
+
+# custom exception
+class MyError(Exception):
+    pass
+
+
+try:
+    raise MyError("Something went wrong!")
+except MyError as e:
+    print("Caught custom error:", e)
+
+
+# implementation
 def valueChecker(text):
-    if(a==""):
-        raise ValueError("\nString cannot be empty") #throws an exception if the user enters nothing
+    if a == "":
+        raise ValueError(
+            "\nString cannot be empty"
+        )  # throws an exception if the user enters nothing
     else:
-        return "\nEntered string is \""+a+"\""
+        return '\nEntered string is "' + a + '"'
+
 
 a = input("Enter any string: ")
-try:                            #the main logic is in try block
+try: 
     print(valueChecker(a))
-except ValueError as error:     #if  any error occurs in try block, it will be caught by except block and handled accordingly.
+except (ValueError) as error: 
     print(error)
-else:                           #this block will execute if no errors occur in the try block. It's optional.
+else:  # this block will execute if no errors occur in the try block. It's optional.
     print("Program Executed successfully")
 finally:
     print("This block always executes whether an exception occurred or not.")
--- 27_Number_guessing_game.py
+++ 27_Number_guessing_game.py
@@ -4,33 +4,35 @@
 
 import math
 import random
+operation = "y"
+while operation == "y" or operation == "Y":
+    lower = int(input(f"Enter the lower limit: "))
+    upper = int(input(f"Enter the upper limit (greater than {lower}): "))
+    num = random.randint(lower,upper)
 
-lower = int(input(f"Enter the lower limit: "))
-upper = int(input(f"Enter the upper limit (greater than {lower}): "))
-num = random.randint(lower,upper)
+    #The chances varies from the range you are selecting so math.log() is used
+    chances = round(math.log(upper-lower+1,2))
+    print("You have got", chances, "Chances")
 
-#The chances varies from the range you are selecting so math.log() is used
-chances = round(math.log(upper-lower+1,2))
-print("You have got", chances, "Chances")
-
-count = 0
-
-while count<chances:
-    guess = int(input("Enter the guess: "))
-    if guess == num :
-        print(f"Congratulations! You won. The number is ", num)
-        break
-    else:
-        if (guess<lower or guess>upper):
-            print("The guess exceed the upper limit and lower limit")
-        elif guess<num:
-            print("The number is greater than the guess")
+    count = 0
+    while count<chances:
+        guess = int(input("Enter the guess: "))
+        if guess == num :
+            print(f"Congratulations! You won. The number is ", num)
+            break
         else:
-            print("The number is less than the guess")
+            if (guess<lower or guess>upper):
+                print("The guess exceed the upper limit and lower limit")
+            elif guess<num:
+                print("The number is greater than the guess")
+            else:
+                print("The number is less than the guess")
 
-        if(count<chances-1) :
-            print("You have ", chances-count-1, " chances left")
-        else:
-            print("Sorry! You lost. Better luck  next time.")
-            
-        count += 1
+            if(count<chances-1) :
+                print("You have ", chances-count-1, " chances left")
+            else:
+                print("Sorry! You lost. Better luck  next time.")
+                print("The number was: ", num)
+                
+            count += 1
+    operation = input("press Y to continue or N to quiet: ")
